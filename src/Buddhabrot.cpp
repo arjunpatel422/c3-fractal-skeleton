0bfb464ed9395ff7118504dae914ffc68f750b6d
arjunpatel422 <arjunpatel422@gmail.com>
2014-03-24 01:25:56 -0700

    version 1.0
    Mandelbrot is working. I've begun steps to attempt to parallelize it. I
    finally fixed all compilation errors.

--- src/Buddhabrot.cpp
+++ src/Buddhabrot.cpp
@@ -3,11 +3,35 @@ using namespace std;
 
 void Buddhabrot::gen_fractal()
 {
-
-   
 	// Real (-2.5, 1)
 	// Imaginary (-1, 1)
-    
+
+	int height = get_height(), width = get_width();
+    int i, npixels = height*width;
+	int* outerPixelBucket = (int *)malloc(height*width*4*sizeof(int));
+    for (i = 0; i<npixels; i++)
+    {
+		int* innerPixelBucket = (int *)malloc(height*width*4*sizeof(int));
+		int n=1,max_iter=MAXITER;
+		double z=0,z_r=0,z_i=0,z_temp=0;
+        int x = i%height;
+        int y = i/height;
+        double c_r = (((double)x)/width)*3.0 - 2.25;
+        double c_i = (((double)y)/height)*3.0 - 1.5;
+		while(n<MAXITER && z<4)
+		{
+			z_temp = pow(z_r,2) - pow(z_i,2) + c_r;//*
+			z_i = z_r*z_i*2 + c_i;
+			z_r = z_temp;
+			z=pow(z_r,2)+pow(z_i,2);
+			n++;
+		}
+		if(z>=4)
+		{
+			
+		}
+		free(innerPixelBucket);
+    }
     // Initialize a bucket array (one integer for each pixel) (this is the outer bucket array)
 
     // iterate over the following several thousand times (at least more times than # of pixels)
@@ -28,7 +52,7 @@ void Buddhabrot::gen_fractal()
      // Which are merged after the computation has finished.
      
      // Parallelizing is not required, but will save you a lot of time.
-     
+     free(outerPixelBucket);
 
 }
     
--- src/Mandelbrot.cpp
+++ src/Mandelbrot.cpp
@@ -1,21 +1,56 @@
 #include "Mandelbrot.h"
 
+void Mandelbrot::color(int x, int y, int r, int g, int b)
+{
+   m_bitmap[4*(x*m_height + y)] = r;
+   m_bitmap[4*(x*m_height + y) + 1] = g;
+   m_bitmap[4*(x*m_height + y) + 2] = b;
+   m_bitmap[4*(x*m_height + y) + y*4 + 3] = 255;
+}
+
+
 void Mandelbrot::gen_fractal()
 {
-	// Real (-2.5, 1)
-	// Imaginary (-1, 1)
-    
-    // Iterate over each pixel and determine the corresponding complex coordinate (or several complex coordinates if you're anti-aliasing)
-    
+	int height = get_height();
+	int width = get_width();
+	int MAX = 1000;
+	int i,j;
+	double z_r,z_i,a,b,c_r,c_i,temp,n;
+	//iterate through rows
+	for (i = 1; i < height; i++)
+	{
+		b=i;
+		a=1;
+		//columns
+		for (j = 1; j < width; j++)
+		{
+			z_r = 0.0;
+			z_i = 0.0;
+		
+			//scale pixels
 
-        // For each pixel, compute the corresponding complex coordinate C (or multiple sub-coordinates, for AA)   
+			c_r = (a/height)*3 - 2.25;
+			c_i = (b/width)*3 - 1.5;
+	
+			n = 0;  //iterator
+			while (z_r*z_r + z_i*z_i < 4 && n < MAX)
+			{
+				temp = z_r;
+				z_r = z_r*z_r - z_i*z_i + c_r;
+				z_i = z_i*temp*2 + c_i;
+				n++;
+			}
+			if (n > MAX ) /* if not converging to infinity (COLOR BLACK) */
+			{
+			 color(i,j,0,0,0);
+			}
+			else
+			{
+			 color(i,j,(255-(pow((double)n/(double)MAX,-0.9)*255)),(255-(pow((double)n/(double)MAX,-0.8)*255)),(255-(pow((double)n/(double)MAX,-0.7)*255)));
+			}
+			a++;
+		}
 
-        // let z_r and z_i be 0
-        // Begin iterating... while z is not infinity and not too many iterations have passed...
-        
-            // Z = Z^2 + C
-            // increment an iteration counter
+	}
 
-        // Color each pixel...
-
-}
+}   
